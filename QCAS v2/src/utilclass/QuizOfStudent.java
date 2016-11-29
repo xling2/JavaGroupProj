06cbf31fc52b3d4acbabc1f1a67205b4989b0d27
xling2 <lingxingyu@192.168.1.10>
2016-11-29 15:07:12 +1030

    Updated QuizOfStudent

--- QCAS v2/src/utilclass/QuizOfStudent.java
+++ QCAS v2/src/utilclass/QuizOfStudent.java
@@ -3,68 +3,73 @@ package utilclass;
 import java.util.Date;
 
 public class QuizOfStudent {
-	public int quizDifficulty;
-	public String quizId;
-	public String studentName;
-	public Question[] questionsOfQuiz;
-	public Answer[] answerOfStudent;
-	public Date startDate;
-	public Date finishDate;
-	public String duration;
-	public int totalScore;
-	public int[] scoreOfDifficulty = new int[3];
 
-	public QuizOfStudent(String student, Question[] questions, Answer[] answerOfStudent, Date startDate,
-			Date finishDate) {
-		// TODO Auto-generated constructor stub
-		this.studentName = student;
-		this.questionsOfQuiz = questions;
-		this.startDate = startDate;
-		this.answerOfStudent = answerOfStudent;
-		this.finishDate = finishDate;
-		getQuizScore();
-		getDuration();
-	}
+    public int quizDifficulty;
+    public String quizId;
+    public String studentName;
+    public Question[] questionsOfQuiz;
+    public Answer[] answerOfStudent;
+    public Date startDate;
+    public Date finishDate;
+    public String duration;
+    public int totalScore;
+    public int[] scoreOfDifficulty = new int[3];
 
-	public void finishQuiz() {
-		this.finishDate = new Date();
-		getDuration();
-		getQuizScore();
-	}
+    public QuizOfStudent(String student, Question[] questions, Answer[] answerOfStudent, Date startDate,
+            Date finishDate) {
+        // TODO Auto-generated constructor stub
+        this.studentName = student;
+        this.questionsOfQuiz = questions;
+        this.startDate = startDate;
+        this.answerOfStudent = answerOfStudent;
+        this.finishDate = finishDate;
+        getQuizScore();
+        getDuration();
+    }
 
-	private void getQuizScore() {
-		int[] totalNumber = new int[] { 0, 0, 0 };
-		int[] correctNumber = new int[] { 0, 0, 0 };
-		for (int i = 0; i < answerOfStudent.length; i++) {
-			Answer answer = answerOfStudent[i];
-			Question question = questionsOfQuiz[i];
-			totalNumber[question.questionDifficult]++;
-			if (question.correctAnswer.equals(answer.toString())) {
-				correctNumber[question.questionDifficult]++;
-			}
-		}
-		this.totalScore = (int) ((float) total(correctNumber) / (float) total(totalNumber) * 100.0);
-		for (int i = 0; i < 3; i++) {
-			double x = (float) correctNumber[i] / (float) (totalNumber[i] == 0 ? 1 : totalNumber[i]) * 100.0;
-			this.scoreOfDifficulty[i] = (int) x;
-		}
-	}
+    public void finishQuiz() {
+        this.finishDate = new Date();
+        getDuration();
+        getQuizScore();
+    }
 
-	private void getDuration() {
-		long ms = finishDate.getTime() - startDate.getTime();
-		int hour = (int) ms / 1000 / 60 / 60;
-		ms -= 60 * 60 * 1000 * hour;
-		int mm = (int) ms / 1000 / 60;
-		ms -= 60 * 1000 * mm;
-		int ss = (int) (ms) / 1000;
-		this.duration = String.format("%02d:%02d:%02d", hour, mm, ss);
-	}
+    private void getQuizScore() {
+        int[] totalNumber = new int[]{0, 0, 0};
+        int[] correctNumber = new int[]{0, 0, 0};
+        for (int i = 0; i < answerOfStudent.length; i++) {
+            Answer answer = answerOfStudent[i];
+            Question question = questionsOfQuiz[i];
+            totalNumber[question.questionDifficult]++;
+            if (question.questionType == 2) {
+                if (answer.toString().equals(question.correctAnswer.toLowerCase())) {
+                    correctNumber[question.questionDifficult]++;
+                }
+            } else if (question.correctAnswer.equals(answer.toString())) {
+                correctNumber[question.questionDifficult]++;
+            }
+        }
+        this.totalScore = (int) ((float) total(correctNumber) / (float) total(totalNumber) * 100.0);
+        for (int i = 0; i < 3; i++) {
+            double x = (float) correctNumber[i] / (float) (totalNumber[i] == 0 ? 1 : totalNumber[i]) * 100.0;
+            this.scoreOfDifficulty[i] = (int) x;
+        }
+    }
 
-	public static int total(int[] x) {
-		int j = 0;
-		for (int i = 0; i < x.length; i++) {
-			j += x[i];
-		}
-		return j;
-	}
+    private void getDuration() {
+        long ms = finishDate.getTime() - startDate.getTime();
+        int hour = (int) ms / 1000 / 60 / 60;
+        ms -= 60 * 60 * 1000 * hour;
+        int mm = (int) ms / 1000 / 60;
+        ms -= 60 * 1000 * mm;
+        int ss = (int) (ms) / 1000;
+        this.duration = String.format("%02d:%02d:%02d", hour, mm, ss);
+    }
+
+    public static int total(int[] x) {
+        int j = 0;
+        for (int i = 0; i < x.length; i++) {
+            j += x[i];
+        }
+        return j;
+    }
 }

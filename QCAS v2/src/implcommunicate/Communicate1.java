b8093413c84b6ddecfd03fcb03bf4953fad7dae0
me <me@yuhao>
2016-12-02 09:20:04 +1030

    java doc

--- QCAS v2/src/implcommunicate/Communicate1.java
+++ QCAS v2/src/implcommunicate/Communicate1.java
@@ -10,53 +10,62 @@ import java.sql.Statement;
 import java.util.Set;
 import java.util.HashSet;
 
+/**
+ *
+ * @author Yuhao HE
+ */
 public class Communicate1 {
     private String resUrl = "jdbc:derby:QuizDB;create = true";
-    private DBConnection con = new DBConnection();
     private String quizUsername = "quiz";
     private String quizPassword = "quiz";
-    private Set <String> SandrewID = new HashSet();
+    private DBConnection con = new DBConnection(); // to do the calculation
+    private Set <String> SandrewID = new HashSet(); // to get the andrewID of users
     
+    /**
+     * This constructor is to get the HashSet of andrewID, and do the calculation
+     * at the time we have an object of Communicate1
+     */
     public Communicate1(){
         SandrewID = con.getAndrewID();
         con.getResult(SandrewID);
     }
  
+    /**
+     *
+     * @return an array of andrewID of students who failed last month
+     */
     public String[] getStudentFailedListOfLastMouth() {
-        
-        /*System.out.println("SAndreID 1: ");
-        for (String a : SandrewID)
-            System.out.println(a);*/
         ArrayList <String> list = con.getStudentFailLM();
         String[] stringArray = list.toArray(new String[list.size()]);
        
         return stringArray; 
     }
 
-
+    /**
+     *
+     * @return an array of andrewID of students who failed last quarter
+     */
     public String[] getStudentFailedListOfLastQuarter() {
-        
-        /*System.out.println("SAndreID 2: ");
-        for (String a : SandrewID)
-            System.out.println(a);*/
         ArrayList <String> list = con.getStudentFailLQ();
         String[] stringArray = list.toArray(new String[list.size()]);
            
         return stringArray;
     }
 
-
+    /**
+     *
+     * @return an array of andrewID of students who failed last year
+     */
     public String[] getStudentFailedListOfLastYear() {
-   
-        /*System.out.println("SAndreID 3: ");
-        for (String a : SandrewID)
-            System.out.println(a);*/
         ArrayList <String> list = con.getStudentFailLY();
         String[] stringArray = list.toArray(new String[list.size()]);
         return stringArray;
     }
 
-  
+    /**
+     *
+     * @return an array of andrewID of students who passed last month
+     */
     public String[] getStudentPassedListOfLastMouth() {
         
         ArrayList <String> list = con.getStudentPassLM();
@@ -64,7 +73,10 @@ public class Communicate1 {
         return stringArray;
     }
 
-   
+    /**
+     *
+     * @return an array of andrewID of students who passed last quarter
+     */
     public String[] getStudentPassedListOfLastQuarter() {
         
         ArrayList <String> list = con.getStudentPassLQ();
@@ -72,6 +84,10 @@ public class Communicate1 {
         return stringArray;
     }
 
+    /**
+     *
+     * @return an array of andrewID of students who passed last year
+     */
     public String[] getStudentPassedListOfLastYear() {
         
         ArrayList <String> list = con.getStudentPassLY();
@@ -80,7 +96,10 @@ public class Communicate1 {
         return stringArray;
     }
 
-
+    /**
+     *
+     * @return an array of the number of quizzes over three periods
+     */
     public int[] getLastNumberOfALLQuizzes() {
        
         int numberTestLM = con.getNumberTestLM();
@@ -90,6 +109,10 @@ public class Communicate1 {
         return numberOfTest;
     }
 
+    /**
+     *
+     * @return the average scores of quizzes over three periods
+     */
     public int[] getlastAverageOfALLQuizzes() {     
       
         int avgScoreLM = con.getAvgScoreLM();
@@ -99,6 +122,10 @@ public class Communicate1 {
         return avgAll;
     }
 
+    /**
+     *
+     * @return average scores of easy level questions over three periods
+     */
     public int[] geteasyAverageScoreOfThreeLastTime() {
         
         int avgScoreELM = con.getAvgScoreELM();
@@ -108,7 +135,10 @@ public class Communicate1 {
         return scoreEasy;
     }
 
-
+    /**
+     *
+     * @return  average scores of medium level questions over three periods
+     */
     public int[] getMediumAverageScoreOfThreeLastTime() {
         
         int avgScoreMLM = con.getAvgScoreMLM();
@@ -119,7 +149,10 @@ public class Communicate1 {
         return scoreMedium;
     }
 
-
+    /**
+     *
+     * @return  average scores of hard level questions over three periods
+     */
     public int[] getHardAverageScoreOfThreeLastTime() {
         
         int avgScoreHLM = con.getAvgScoreHLM();
@@ -130,6 +163,10 @@ public class Communicate1 {
         return scoreHard;
     }
 
+    /**
+     *
+     * @return  average scores of three levels questions in last month
+     */
     public int[] getAllQuizAverageScoreOfEachDifficultyInLastMouth() {
         
         int avgScoreELM = con.getAvgScoreELM();
@@ -140,7 +177,10 @@ public class Communicate1 {
         return scoreDiffLM;
     }
 
- 
+    /**
+     *
+     * @return average scores of three levels questions in last quarter
+     */
     public int[] getAllQuizAverageScoreOfEachDifficultyInLastQuater() {
     
         int avgScoreELQ = con.getAvgScoreELQ();
@@ -151,6 +191,10 @@ public class Communicate1 {
         return scoreDiffLQ;
     }
 
+    /**
+     *
+     * @return average scores of three levels questions in last year
+     */
     public int[] getAllQuizAverageScoreOfEachDifficultyInLastYear() {
       
         int avgScoreELY = con.getAvgScoreELY();
@@ -161,13 +205,16 @@ public class Communicate1 {
         return scoreDiffLY;
     }
 
-
+    /**
+     *This method is to delete questions in question bank
+     * @param questionID is the unique ID of each question in question bank
+     */
     public void deleteById(int questionID) {
        String sql = "DELETE FROM QUESTION WHERE NUMBER = " + questionID;  
        try (Connection con = DriverManager.getConnection(resUrl, quizUsername, quizPassword)){
            Statement stmt = con.createStatement();
            int i = stmt.executeUpdate(sql);
-           //check
+           //check if successfully delete a question
            if (i != 1) {
             System.out.println ("Failed to delete a question record");
            } 
